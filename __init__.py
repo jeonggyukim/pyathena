2d1c72020eace9df1d7796780397821474a5984b
Chang-Goo Kim <changgoo@princeton.edu>
2018-02-15 11:22:49 -0500

    revise indentation, python3 compatible

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from pyathena.parse_par import parse_par
+from pyathena.parse_par import *
 from pyathena.set_units import set_units
 from pyathena.ath_hst import read_w_pandas as hst_reader
 from pyathena.cooling import coolftn
