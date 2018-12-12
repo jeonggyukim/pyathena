ee6c90e5cc6b1d24283a59aa96aca43c6f60e4b2
Jeong-Gyu Kim <omyojj@gmail.com>
2018-12-12 16:59:18 -0500

    Add post-processing H2. Does not work with ionizing radiation.

--- tigradpy/__init__.py
+++ tigradpy/__init__.py
@@ -7,8 +7,7 @@ __all__ = ["add_fields",
            "read_starpar_vtk",
            "read_zprof",
            "read_zprof_all",
-           "units",
-           "yt_multipanel"]
+           "units"]
 
 from .add_fields import add_fields
 from .mass_to_lum import mass_to_lum
@@ -19,5 +18,5 @@ from io.read_athinput import read_athinput
 from io.read_hst import read_hst
 from io.read_starpar_vtk import read_starpar_vtk
 from io.read_zprof import read_zprof,read_zprof_all
-from vis.plt_multipanel import yt_multipanel
+#from vis.plt_multipanel import yt_multipanel
 from util.units import units
