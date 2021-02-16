dcb16bcb83c5262906f75dd654a2f89c1e2d8360
Jeong-Gyu Kim <omyojj@gmail.com>
2021-02-16 10:23:30 -0500

    Add script for reading grackle cooling

---
+++ pyathena/microphysics/cool_grackle.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+
+def cool_grackle(Z=1.0):
+    """
+    Function to read equilibrium cooling function in grackle
+
+    Parameters
+    ----------
+    Z : float
+        Metallicity
+    """
+    
+    dirname = os.path.dirname(__file__)
+    data_dir = os.path.join(dirname, '../../data/microphysics')
+    fname = os.path.join(data_dir, 'Grackle_equillibrium_cooling_{0:6.4f}Z.dat'.format(Z))
+    df = pd.read_csv(fname, sep=' ')
+    
+    return df
