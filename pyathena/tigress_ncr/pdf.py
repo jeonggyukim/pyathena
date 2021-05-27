025487d81c4065b4d5d402269ae57aae065ffa4a
Jeong-Gyu Kim <kimjg@astro.princeton.edu>
2021-05-27 11:39:57 -0400

    Improve tigress_ncr.pdf script

--- pyathena/tigress_ncr/pdf.py
+++ pyathena/tigress_ncr/pdf.py
@@ -17,61 +17,103 @@ from ..plt_tools.plt_starpar import scatter_sp
 
 class PDF:
     
-    bins=dict(nH=np.logspace(-5,3,81),
-              nHI=np.logspace(-2,5,71),
-              nH2=np.logspace(-2,5,71),
-              xH2=np.linspace(0,0.5,51),
-              nHII=np.logspace(-5,3,101),
-              T=np.logspace(1,8,141),
-              pok=np.logspace(0,7,71),
-              chi_PE=np.logspace(-3,4,71),
-              chi_FUV=np.logspace(-3,4,71),
-              Lambda_cool=np.logspace(-30,-20,101),
-              xi_CR=np.logspace(-17,-15,41)
+    bins=dict(nH=np.logspace(-5,3,161),
+              nHI=np.logspace(-2,5,141),
+              nH2=np.logspace(-2,5,141),
+              xH2=np.linspace(0,0.5,101),
+              nHII=np.logspace(-5,3,201),
+              xe=np.logspace(-5,np.log10(2),201),
+              T=np.logspace(1,8,281),
+              pok=np.logspace(0,7,141),
+              chi_PE=np.logspace(-3,4,141),
+              chi_FUV=np.logspace(-3,4,141),
+              Lambda_cool=np.logspace(-30,-20,201),
+              xi_CR=np.logspace(-17,-14,121)
     )
-    
+
+    @LoadSim.Decorators.check_pickle
+    def read_pdf2d_avg(self, nums=None, savdir=None, force_override=False):
+        """Take sum of all pdf2d
+        """
+
+        if nums is None:
+            nums = self.nums
+
+        rr = dict()
+        print('[read_pdf2d_avg]:', end=' ')
+        for i,num in enumerate(nums):
+            print(num, end=' ')
+            r = self.read_pdf2d(num, force_override=False)
+            if i == 0:
+                for k in r.keys():
+                    if k == 'time_code':
+                        rr[k] = []
+                    else:
+                        rr[k] = dict()
+                        rr[k]['xe'] = r[k]['xe']
+                        rr[k]['ye'] = r[k]['ye']
+                        rr[k]['H'] = np.zeros_like(r[k]['H'])
+                        rr[k]['Hw'] = np.zeros_like(r[k]['Hw'])
+
+            for k in r.keys():
+                if k == 'time_code':
+                    rr[k].append(r[k])
+                else:
+                    rr[k]['H'] += r[k]['H']
+                    rr[k]['Hw'] += r[k]['Hw']
+
+        return rr    
+
     @LoadSim.Decorators.check_pickle
     def read_pdf2d(self, num,
-                   bin_fields=None, bins=None, prefix='pdf2d',
+                   bin_fields=None, weight_fields_def=None,
+                   bins=None, prefix='pdf2d',
                    savdir=None, force_override=False):
         if self.par['configure']['radps'] == 'ON':
             bin_fields_def = [['nH', 'pok'], ['nH', 'T'], ['nH','chi_FUV'],
-                              ['T','Lambda_cool'], ['nH','xi_CR'], ['nH', 'xH2']]
+                              ['T','Lambda_cool'], ['nH','xi_CR'], ['nH', 'xH2'],
+                              ['T','xe']]
+            weight_fields_def = ['nH','nH','nH',
+                                 'cool_rate','nH','nH','cool_rate']
         else:
             bin_fields_def = [['nH', 'pok'], ['nH', 'T']]
+            weight_fields_def = ['nH', 'nH']
+            
         if bin_fields is None:
             bin_fields = bin_fields_def
+            weight_fields = weight_fields_def
 
         ds = self.load_vtk(num=num)
         res = dict()
         
-        for bf in bin_fields:
+        for bf,wf in zip(bin_fields,weight_fields):
             k = '-'.join(bf)
             res[k] = dict()
             dd = ds.get_field(bf)
             xdat = dd[bf[0]].data.flatten()
             ydat = dd[bf[1]].data.flatten()
-            # Volume weighted hist
-            weights = None
+            # Unweighted hist (volume-weighted)
             H, xe, ye = np.histogram2d(xdat, ydat, (self.bins[bf[0]], self.bins[bf[1]]),
-                                       weights=weights)
+                                       weights=None)
             res[k]['H'] = H
             res[k]['xe'] = xe
             res[k]['ye'] = ye
-            
-            # Density weighted hist
-            weights = (ds.get_field('nH'))['nH'].data.flatten()
+
+            # Weighted hist
+            weights = (ds.get_field(wf))[wf].data.flatten()
             Hw, xe, ye = np.histogram2d(xdat, ydat, (self.bins[bf[0]], self.bins[bf[1]]),
                                         weights=weights)
             res[k]['Hw'] = Hw
 
+        res['time_code'] = ds.domain['time']
+        
         return res
 
     def plt_pdf2d(self, ax, dat, bf='nH-pok',
                   cmap='cubehelix_r',
                   norm=mpl.colors.LogNorm(1e-6,2e-2),
                   kwargs=dict(alpha=1.0, edgecolor='face', linewidth=0, rasterized=True),
-                  weighted=True,
+                  weighted=True, 
                   xscale='log', yscale='log'):
         
         if weighted:
@@ -79,13 +121,14 @@ class PDF:
         else:
             hist = 'H'
 
-        ax.pcolormesh(dat[bf]['xe'], dat[bf]['ye'], dat[bf][hist].T/dat[bf][hist].sum(),
-                      norm=norm, cmap=cmap, **kwargs)
+        c = ax.pcolormesh(dat[bf]['xe'], dat[bf]['ye'], dat[bf][hist].T/dat[bf][hist].sum(),
+                          norm=norm, cmap=cmap, **kwargs)
 
         kx, ky = bf.split('-')
         ax.set(xscale=xscale, yscale=yscale,
                xlabel=self.dfi[kx]['label'], ylabel=self.dfi[ky]['label'])
-            
+        return c
+    
     def plt_pdf2d_all(self, num, suptitle=None, savdir=None,
                       plt_zprof=True, savdir_pkl=None,
                       force_override=False, savefig=True):
@@ -114,13 +157,19 @@ class PDF:
         s.plt_pdf2d(axes[1,0], pdf, 'nH-pok', weighted=True)
         s.plt_pdf2d(axes[0,1], pdf, 'nH-chi_FUV', weighted=False)
         s.plt_pdf2d(axes[1,1], pdf, 'nH-chi_FUV', weighted=True)
-        s.plt_pdf2d(axes[0,2], pdf, 'T-Lambda_cool', weighted=False)
-        s.plt_pdf2d(axes[1,2], pdf, 'T-Lambda_cool', weighted=True)
+        try:
+            s.plt_pdf2d(axes[0,2], pdf, 'T-Lambda_cool', weighted=False)
+            s.plt_pdf2d(axes[1,2], pdf, 'T-Lambda_cool', weighted=True)
+        except:
+            pass
         s.plt_pdf2d(axes[0,3], pdf, 'nH-xi_CR', weighted=False)
         s.plt_pdf2d(axes[1,3], pdf, 'nH-xi_CR', weighted=True)
 
         ax = axes[2,0]
-        s.plt_proj(ax, prj, 'z', 'Sigma_gas')
+        # s.plt_proj(ax, prj, 'z', 'Sigma_gas')
+        ax.imshow(prj['z']['Sigma_gas'], cmap='pink_r',
+                  extent=prj['extent']['z'], norm=mpl.colors.LogNorm(),
+                  origin='lower', interpolation='none')
         scatter_sp(sp, ax, 'z', kind='prj', kpc=False, norm_factor=5.0, agemax=20.0)
         ax.axes.xaxis.set_visible(False) ; ax.axes.yaxis.set_visible(False)
         ax.set(xlim=(ds.domain['le'][0], ds.domain['re'][0]),
@@ -130,7 +179,8 @@ class PDF:
         s.plt_slice(ax, slc, 'z', 'chi_FUV', norm=LogNorm(1e-1,1e2))
         scatter_sp(sp, ax, 'z', kind='slc', dist_max=50.0, kpc=False, norm_factor=5.0, agemax=20.0)
         ax.axes.xaxis.set_visible(False) ; ax.axes.yaxis.set_visible(False)
-        ax.set(xlim=(-512,512),ylim=(-512,512))
+        ax.set(xlim=(ds.domain['le'][0], ds.domain['re'][0]),
+               ylim=(ds.domain['le'][1], ds.domain['re'][1]))
 
         if plt_zprof:
             ax = axes[2,2]
@@ -150,20 +200,20 @@ class PDF:
         # ax2 = fig.add_subplot(gs[2, 5])
 
         # ax = ax1
-        # s.plt_proj(ax, prj, 'y', 'Sigma_gas')
-        # pa.scatter_sp(sp, ax, 'y', kind='prj', kpc=False, norm_factor=20.0, agemax=20.0)
-        # ax.axes.xaxis.set_visible(False) ; ax.axes.yaxis.set_visible(False)
+        s.plt_proj(ax, prj, 'y', 'Sigma_gas')
+        scatter_sp(sp, ax, 'y', kind='prj', kpc=False, norm_factor=20.0, agemax=20.0)
+        ax.axes.xaxis.set_visible(False) ; ax.axes.yaxis.set_visible(False)
 
         # ax = ax2
-        # s.plt_slice(ax, slc, 'y', 'chi_FUV', norm=LogNorm(1e-1,1e2))
-        # pa.scatter_sp(sp, ax, 'y', kind='slc', kpc=False, norm_factor=20.0, agemax=20.0)
-        # ax.axes.xaxis.set_visible(False) ; ax.axes.yaxis.set_visible(False)
+        s.plt_slice(ax, slc, 'y', 'chi_FUV', norm=LogNorm(1e-1,1e2))
+        scatter_sp(sp, ax, 'y', kind='slc', kpc=False, norm_factor=20.0, agemax=20.0)
+        ax.axes.xaxis.set_visible(False) ; ax.axes.yaxis.set_visible(False)
 
         ax = axes[2,3]
         ax.semilogy(hst['time_code'],hst['sfr10'])
         ax.semilogy(hst['time_code'],hst['sfr40'])
         ax.axvline(s.domain['time'], color='grey', lw=0.75)
-        ax.set(xlabel='time [code]', ylabel=r'$\Sigma_{\rm SFR}$', ylim=(1e-3,1e0))
+        ax.set(xlabel='time [code]', ylabel=r'$\Sigma_{\rm SFR}$', ylim=(1e-4,1e-1))
 
         if suptitle is None:
             suptitle = self.basename
