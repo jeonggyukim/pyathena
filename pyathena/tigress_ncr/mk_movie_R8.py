4b3e9779617174fb5261ab7cdf0e3d53a652d844
Chang-Goo Kim <cgkim@astro.princeton.edu>
2022-01-13 11:08:26 -0500

    Added new scripts

---
+++ pyathena/tigress_ncr/mk_movie_R8.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+import os.path as osp
+import gc
+import time
+import matplotlib.pyplot as plt
+import pprint
+import argparse
+import sys
+
+from pyathena.plt_tools.make_movie import make_movie
+
+basedir = '/tigress/changgoo/TIGRESS-R8/combined_slices/'
+basedir = '/tigress/changgoo/TIGRESS-NCR/R8_combined_figures/'
+basedir = '/tigress/changgoo/TIGRESS-NCR/LGR4_combined_figures/'
+if not osp.isdir(osp.join(basedir,'movies')): os.mkdir(osp.join(basedir,'movies'))
+
+for field in ['Sigma_gas']:#,'nH','pok','surf']:
+    fin = osp.join(basedir, '{0}/{0}.*.png'.format(field))
+    fout = osp.join(basedir, 'movies/{0:s}.mp4'.format(field))
+    make_movie(fin, fout, fps_in=15, fps_out=15)
+    from shutil import copyfile
