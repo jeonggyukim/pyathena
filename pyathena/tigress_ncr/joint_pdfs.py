273cc1fce0348040b1c623896f89ea3cbf4b6e90
Chang-Goo Kim <changgoo@princeton.edu>
2022-01-19 15:24:40 -0500

    Update joint_pdfs using new pdf class

--- pyathena/tigress_ncr/joint_pdfs.py
+++ pyathena/tigress_ncr/joint_pdfs.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import argparse
 import pyathena as pa
+import gc
 
 def calc_pdf(data,xf,yf,wf=None,logs=dict(),dbins=dict(),ranges=dict()):
     xdata = data[xf].data.flatten()
@@ -64,7 +65,7 @@ def jointpdf(ds,par,zmin=0,zmax=300,fields='auto',wfields=['nH'],verbose=False):
     nologs = ['vz']
     ranges = dict(nH=(-5,3),T=(1,8),pok=(0,8),vz=(-500,500),
                   chi_FUV=(-2,4),Erad_LyC=(-30,-10),Uion=(-20,5),
-                  Lambda_cool=(-30,-20),xH2=(-5,0),xHI=(-5,0),xHII=(-5,0),ne=(-5,3))
+                  Lambda_cool=(-30,-18),xH2=(-5,0),xHI=(-5,0),xHII=(-5,0),ne=(-5,3))
     dbins = dict(vz=10)
 
     if verbose: print('time = {}'.format(ds.domain['time']))
@@ -147,6 +148,67 @@ def plot_pair(pdf,wf='vol',fields=None):
         plt.xlabel(xf)
         plt.ylabel(yf)
 
+def calc_pdfs(ds,xf='T',yf='Lambda_cool',zmin=0,zmax=300,force_override=False,
+              cooling=False,species=False,radiation=False):
+    """calculate PDFs for given x and y with a variety of weight fields
+    outputs are stored in individual folders for later uses
+
+    Usage
+    =====
+
+    >>> sim = pa.LoadSim(path)
+    >>> ds = sim.load_vtk(num)
+    >>> calc_pdfs(ds,xf=xf,yf=yf)
+    """
+
+    pdf_z = dict()
+    pdf_tot = dict()
+
+    tot_volume = -1
+    tot_mass = -1
+
+    wfields = [None,'nH']
+    if cooling: 
+        wfields += ['cool_rate','heat_rate','net_cool_rate']
+        tot_cool_rate = -1
+    if species: wfields += ['nHI','nHII','nH2','ne']
+    if radiation: 
+        wfields += ['xi_CR','rad_energy_density_PE','rad_energy_density_LW','rad_energy_density_PH']
+    for zmax,pdf_dict in zip([ds.domain['re'][2],zmax],[pdf_tot,pdf_z]):
+        pdfdict = dict()
+        for wf in wfields:
+            pdf=s.load_one_jointpdf(xf,yf,wf,ds,
+                                    zrange=(zmin,zmax),
+                                    force_override=force_override)
+            pdfdict['vol' if wf is None else wf] = pdf
+        x = pdf.dims[1]
+        y = pdf.dims[0]
+        dx = x[1]-x[0]
+        dy = y[1]-y[0]
+        if tot_mass < 0: tot_mass = pdfdict['nH'].sum().data*dx*dy
+        if tot_volume < 0: tot_volume = np.prod(ds.domain['Nx'])
+        pdf_dict['volume']=pdfdict['vol']/tot_volume
+        pdf_dict['mass']=pdfdict['nH']/tot_mass
+
+        if cooling and (tot_cool_rate < 0): 
+            tot_cool_rate = pdfdict['cool_rate'].sum().data*dx*dy
+            pdf_dict['net_cooling']=pdfdict['net_cool_rate']/tot_cool_rate
+            pdf_dict['cooling']=pdfdict['cool_rate']/tot_cool_rate
+            pdf_dict['heating']=pdfdict['heat_rate']/tot_cool_rate
+
+        if species:
+            pdf_dict['xHI']=pdfdict['nHI']/pdfdict['nH']
+            pdf_dict['xHII']=pdfdict['nHII']/pdfdict['nH']
+            pdf_dict['xH2']=pdfdict['nH2']/pdfdict['nH']
+            pdf_dict['xe']=pdfdict['ne']/pdfdict['nH']
+
+        if radiation:
+            pdf_dict['PE']=pdfdict['radiation_energy_density_PE']
+            pdf_dict['LW']=pdfdict['radiation_energy_density_LW']
+            pdf_dict['PH']=pdfdict['radiation_energy_density_PH']
+
+    return pdf_z, pdf_tot
+
 if __name__ == '__main__':
     basedir_def = '/tigress/changgoo/TIGRESS-NCR/R8_8pc_NCR.full'
     parser = argparse.ArgumentParser()
@@ -159,14 +221,7 @@ if __name__ == '__main__':
     s = pa.LoadSimTIGRESSNCR(basedir, verbose=True)
     for num in s.nums:
         ds = s.load_vtk(num)
-        for zrange in [(0,300)]:#,(300,1000),(1000,2000)]:
-            zmin,zmax = zrange
-            savdir = '{}/jointpdf_z{:02d}-{:02d}/'.format(s.savdir,int(zmin/100),int(zmax/100))
-            if not os.path.isdir(savdir): os.mkdir(savdir)
-            fname = '{}{}.{:04d}.pdf.nc'.format(savdir,s.basename,num)
-            if not os.path.isfile(fname):
-                print("Creating...",end=" ")
-                pdf = jointpdf(ds,s.par,zmin=zmin,zmax=zmax,verbose=False)
-                pdf.to_netcdf(fname)
-                pdf.close()
-            print(fname)
+        pdf_z,pdf_tot = calc_pdfs(ds,'T','Lambda_cool',zmin=0,zmax=300,
+                                  force_override=True,
+                                  cooling=True,species=True,radiation=True)
+        gc.collect()
