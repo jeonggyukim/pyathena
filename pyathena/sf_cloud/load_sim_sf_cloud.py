c06fbbdc336496ef102e8a9873a10c0f049fb82d
Jeong-Gyu Kim <kimjg@astro.princeton.edu>
2022-03-28 11:07:09 -0400

    Update SFR_mean calculation. Takes summary dataframe as an argument

--- pyathena/sf_cloud/load_sim_sf_cloud.py
+++ pyathena/sf_cloud/load_sim_sf_cloud.py
@@ -136,7 +136,7 @@ class LoadSimSFCloud(LoadSim, Hst, StarPar, SliceProj, PDF,
         df['rho'] = cl.rho.cgs.value
         df['nH'] = cl.nH.cgs.value
         df['tff'] = cl.tff.to('Myr').value
-
+        df['tdyn'] = df['R']/df['sigma1d']
         
         if df['mhd']:
             df['muB'] = float(par['problem']['muB'])
@@ -273,15 +273,15 @@ class LoadSimSFCloud(LoadSim, Hst, StarPar, SliceProj, PDF,
             df['t_SF2'] = M_sp_final**2 / \
                         integrate.trapz(h['SFR']**2, h.time)
             df['tau_SF2'] = df['t_SF2']/df['tff']
-            df['SFR_mean'] = get_SFR_mean(h, 0.0, 90.0)['SFR_mean']
+            #df['SFR_mean'] = get_SFR_mean(df, h, 0.0, 90.0)['SFR_mean']
             try:
                 df['SFE_3Myr'] = h.loc[h['time'] > df['t_*'] + 3.0, 'M_sp'].iloc[0]/df['M']
             except IndexError:
                 df['SFE_3Myr'] = np.nan
             # depletion time t_dep = M0/SFR_mean
-            df['t_dep'] = df['M']/df['SFR_mean']
+            #df['t_dep'] = df['M']/df['SFR_mean']
             # SFE per free-fall time eps_ff = tff0/tdep
-            df['eps_ff'] = df['tff']/df['t_dep']
+            #df['eps_ff'] = df['tff']/df['t_dep']
             # Time at which molecular/neutral gas mass < 5% of the initial cloud mass
             try:
                 df['t_mol_5%'] = h.loc[h['M_H2_cl'] < 0.05*df['M'], 'time'].iloc[0]
--- pyathena/sf_cloud/sfr.py
+++ pyathena/sf_cloud/sfr.py
@@ -1,16 +1,25 @@
 from scipy.optimize import curve_fit
 
-def get_SFR_mean(h, t0_SF=0.0, t1_SF=90.0):
+def get_SFR_mean(df, h, t0_SF=0.0, t1_SF=90.0):
     """
-    Perform least-square fitting to SF history (from t_* to t1_SF)
+    Perform linear least-squares fitting to SF history (from t0 to t1)
+
+    Parameters
+    ----------
+    df : pandas dataframe
+       summary returned by s.get_summary()
+    h : pandas dataframe
+       History
+    t0_SF, t1_SF : floats between 0 and 100
+       Set time interval (e.g., for 10 and 90, consider t_10%,* < t < t_90%,*)
 
     Returns
     -------
     Slope (mean SFR) in units of Msun/Myr
     """
 
-    def func(x, a):
-        return a*x
+    def func(x, a, b):
+        return a*x + b
 
     Mstar_final = max(h['M_sp'].values)
 
@@ -21,11 +30,14 @@ def get_SFR_mean(h, t0_SF=0.0, t1_SF=90.0):
         t0 = h['time'][h['M_sp'] > 1e-2*t0_SF*Mstar_final].values[0]
         
     t1 = h['time'][h['M_sp'] > 1e-2*t1_SF*Mstar_final].values[0]
+    print(t0,t1)
+    M_sp_t0 = h.loc[h['time'] == t0, 'M_sp'].values
+    M_sp_t1 = h.loc[h['time'] == t1, 'M_sp'].values
     
     h_ = h[(h['time'] >= t0) & (h['time'] <= t1)]
     xdata = h_['time'].values - t0
     ydata = h_['M_sp'].values
-    SFR_mean, pcov = curve_fit(func, xdata, ydata)
+    popt, pcov = curve_fit(func, xdata, ydata)
 
     #plt.plot(h['time'],h['Mstar'])
     # plt.plot(h_['time'] - rr['t_*'],h_['Mstar'],ls='--')
@@ -34,8 +46,16 @@ def get_SFR_mean(h, t0_SF=0.0, t1_SF=90.0):
     res = dict()
     res['t0'] = t0
     res['t1'] = t1
-    res['SFR_mean'] = SFR_mean[0]
-    res['h'] = h_
+    res['SFR_mean'] = popt[0]
+    res['popt'] = popt
+    #res['h'] = h_
+    res['M_sp_t0'] = M_sp_t0
+    res['M_sp_t1'] = M_sp_t1
+
+    res['tff'] = df['tff']
+    res['tdyn'] = df['tdyn']
+    res['alpha_vir'] = df['alpha_vir']
+    res['eps_ff'] = df['tff']/(df['M']/popt[0])
     
     return res
 
