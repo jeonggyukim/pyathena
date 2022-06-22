54957ac1616e6e162b6531f77f79ca0bc44ae06a
Jeong-Gyu Kim <kimjg@astro.princeton.edu>
2022-06-22 08:44:50 -0400

    Add WD01 LMC, SMC dust model

---
+++ data/kext_albedo_WD_SMCbar_0_README.txt
@@ -0,0 +1,46 @@
+Extinction, scattering, and absorption properties computed for the
+carbonaceous - silicate dust model of Weingartner & Draine (2001) and
+Li & Draine (2001).
+
+Model consists of mixture of carbonaceous grains, and amorphous
+silicate grains.  Carbonaceous grains are PAH-like when small, and
+graphite-like when large (see Li & Draine 2001).  Size distributions
+from Weingartner & Draine (2001).  PAH optical properties are from Li
+& Draine (2001).  Graphite and silicate optical properties from Draine
+& Lee (1984), Laor & Draine (1993), Weingartner & Draine (2001), and
+Li & Draine (2001).
+
+Grain size distribution: Weingartner & Draine (2001) model for
+"SMC bar" dust with C/H = b_C = 0 in two log-normal size
+distributions.
+
+The PAH/graphitic grains in the size distribution given by eq. (5,6)
+of Weingartner & Draine (2001) are assumed to be 100% ionized.
+
+Calculated by B.T. Draine, Princeton University
+
+Thu Oct  3 23:30:39 EDT 2002
+ 
+References:
+Draine, B.T., & Lee, H.-M. 1984, "Optical Properties of Interstellar
+    Graphite and Silicate Grains", Astrophys. J., 285, 89-108
+Laor, A., & Draine, B.T. 1993, "Spectroscopic Constraints on the Properties
+    of Dust in Active Galactic Nuclei", Astrophys. J., 402, 441-468
+Li, A., & Draine, B.T. 2001, "Infrared Emission from Interstellar Dust. II.
+    The Diffuse Interstellar Medium", Astrophys. J., 554, 778-802
+Weingartner, J.C., & Draine, B.T. 2001, "Dust Grain-Size Distributions and 
+    Extinction in the Milky Way, LMC, and SMC", Astrophys. J., 548, 296-309
+
+Tabulated quantities:
+lambda  = wavelength in vacuo (micron)
+albedo  = (scattering cross section)/(extinction cross section)
+g       = <cos(theta)> for scattered light
+C_ext/H = extinction cross section per H nucleon (cm^2/H)
+K_abs   = absorption cross section per mass of dust (cm^2/gram)
+
+ 3.506E-27 = M_dust per H nucleon (gram/H) for this dust model
+ 6.669E+02 = M_gas/M_dust for this dust model
+
+ lambda   albedo    g     C_ext/H    K_abs
+(micron)                  (cm^2/H)  (cm^2/g)
+--------- ------  ------ --------- ---------
--- pyathena/microphysics/dust_draine.py
+++ pyathena/microphysics/dust_draine.py
@@ -46,38 +46,57 @@ class DustDraine(object):
         self.fname_Rv31 = os.path.join(local,'../../data/kext_albedo_WD_MW_3.1A_60_D03.dat')
         self.fname_Rv40 = os.path.join(local, '../../data/kext_albedo_WD_MW_4.0A_40_D03.dat')
         self.fname_Rv55 = os.path.join(local, '../../data/kext_albedo_WD_MW_5.5A_30_D03.dat')
+        self.fname_LMCavg = os.path.join(local, '../../data/kext_albedo_WD_LMCavg_20.dat')
+        self.fname_SMCbar = os.path.join(local, '../../data/kext_albedo_WD_SMCbar_0.dat')
 
         # Gas to dust ratio
         self.GTD = {'Rv31':1.236e2,
                     'Rv40':1.174e2,
-                    'Rv55':1.051e2}
+                    'Rv55':1.051e2,
+                    'LMCavg':4.281E+02,
+                    'SMCbar':6.669E+02,
+                    }
+        
+        self.Mdust_per_H = {'Rv31':1.870E-26,
+                            'Rv40':1.969E-26,
+                            'Rv55':2.199E-26,
+                            'LMCavg':5.462E-27,
+                            'SMCbar':3.506E-27,
+                            }
         
         self.dfa = dict()
         self.dfa['Rv31'] = self._read_data(self.fname_Rv31, self.GTD['Rv31'])
         self.dfa['Rv40'] = self._read_data(self.fname_Rv40, self.GTD['Rv40'])
         self.dfa['Rv55'] = self._read_data(self.fname_Rv55, self.GTD['Rv55'])
+        self.dfa['LMCavg'] = self._read_data(self.fname_LMCavg, self.GTD['LMCavg'], MW=False)
+        self.dfa['SMCbar'] = self._read_data(self.fname_SMCbar, self.GTD['SMCbar'], MW=False)
 
     @staticmethod
-    def _read_data(fname, GTD):
+    def _read_data(fname, GTD, MW=True):
         
         # Read dielectronic recombination rate data
         with open(fname, 'r') as fp:
             lines = fp.readlines()
         
         nline = len(lines)
+
         lwav = np.zeros(nline,)
         albedo = np.zeros(nline,)
         cos = np.zeros(nline,)
         Cext = np.zeros(nline,)
         kappa_abs = np.zeros(nline,)
-        cossq = np.zeros(nline,)
-        
+        if MW:
+            cossq = np.zeros(nline,)
+            idx_comment = 6
+        else:
+            idx_comment = 5
+            
         # Get maximum string length of comments
         maxlen = 0
         for i, line in enumerate(lines):
             l = line.split()
-            if len(l[6:]) != 0:
-                maxlen = max(maxlen, len(' '.join(l[6:])))
+            if len(l[idx_comment:]) != 0:
+                maxlen = max(maxlen, len(' '.join(l[idx_comment:])))
         comment = np.chararray(nline, itemsize=maxlen)
         
         for i, line in enumerate(lines):
@@ -87,17 +106,24 @@ class DustDraine(object):
             cos[i] = float(l[2])
             Cext[i] = float(l[3])
             kappa_abs[i] = float(l[4])
-            cossq[i] = float(l[5])
-            if len(l[6:]) != 0:
-                comment[i] = ' '.join(l[6:])
+            if MW:
+                cossq[i] = float(l[5])
+            if len(l[idx_comment:]) != 0:
+                comment[i] = ' '.join(l[idx_comment:])
             else:
                 comment[i] = ''
 
+        if MW:
+            df = pd.DataFrame({'lwav':lwav, 'albedo':albedo, 'cos':cos,
+                               'Cext':Cext, 'K_abs':kappa_abs, 'cossq':cossq,
+                               'comment':comment})
+        else:
+            df = pd.DataFrame({'lwav':lwav, 'albedo':albedo, 'cos':cos,
+                               'Cext':Cext, 'K_abs':kappa_abs,
+                               'comment':comment})
 
-        df = pd.DataFrame({'lwav':lwav, 'albedo':albedo, 'cos':cos,
-                           'Cext':Cext, 'K_abs':kappa_abs, 'cossq':cossq,
-                           'comment':comment})
-
+        df['Cabs'] = df['Cext']*(1 - df['albedo'])
+            
         df['lAA'] = df['lwav']*1e4
         df['kappa_abs'] = df['K_abs']/GTD
         
