cd2550140ed5878177a5a5774386c8e99adaaed5
Jeong-Gyu Kim <kimjg@astro.princeton.edu>
2022-06-27 23:37:21 -0400

    Add option to load tarred vtk with yt

--- pyathena/load_sim.py
+++ pyathena/load_sim.py
@@ -226,10 +226,20 @@ class LoadSim(object):
                 self.logger.error('load_method "{0:s}" not recognized.'.format(
                     self.load_method) + ' Use either "yt", "pyathena", "pyathena_classic".')
         elif self.fvtk.endswith('tar'):
-            self.ds = AthenaDataSetTar(self.fvtk, units=self.u, dfi=self.dfi)
-            self.domain = self.ds.domain
-            self.logger.info('[load_vtk_tar]: {0:s}. Time: {1:f}'.format(\
-                osp.basename(self.fvtk), self.ds.domain['time']))
+            if self.load_method == 'pyathena':
+                self.ds = AthenaDataSetTar(self.fvtk, units=self.u, dfi=self.dfi)
+                self.domain = self.ds.domain
+                self.logger.info('[load_vtk_tar]: {0:s}. Time: {1:f}'.format(\
+                    osp.basename(self.fvtk), self.ds.domain['time']))
+            elif self.load_method == 'yt':
+                if hasattr(self, 'u'):
+                    units_override = self.u.units_override
+                else:
+                    units_override = None
+                self.ds = yt.load(self.fvtk, units_override=units_override)
+            else:
+                self.logger.error('load_method "{0:s}" not recognized.'.format(
+                    self.load_method) + ' Use either "yt" or "pyathena".')
 
         return self.ds
 
