b718b62ae08635ec9dfb6baef85a335d746f0797
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2023-03-27 11:39:36 -0400

    Add parhst and partab reader

---
+++ pyathena/io/read_partab.py
@@ -0,0 +1,12 @@
+"""
+Read athena++ partab file
+"""
+
+
+from .athena_read import partab
+
+def read_partab(filename, **kwargs):
+    ds = partab(filename, **kwargs)
+    ds.set_index('pid')
+    
+    return ds
