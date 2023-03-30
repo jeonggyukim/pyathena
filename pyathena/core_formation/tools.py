116d53bef35ccde744200b43ad04167a5c847daf
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2023-03-30 12:27:09 -0400

    Add function to calculate radial density and velocity profile

--- pyathena/core_formation/tools.py
+++ pyathena/core_formation/tools.py
@@ -1,6 +1,39 @@
 import numpy as np
+import xarray as xr
 from .tes import TES
+from ..util.transform import to_spherical, groupby_bins
 
+
+def radial_profile(ds, origin, rmax):
+    """Calculates radial density velocity profiles at the selected position
+
+    Args:
+        ds: xarray.Dataset instance containing conserved variables.
+        origin: tuple-like (x0, y0, z0) representing the origin of the spherical coords.
+        rmax: maximum radius to bin.
+
+    Returns:
+        rprof: xarray.Dataset instance containing angular-averaged radial profiles of
+               density, velocities, and velocity dispersions.
+    """
+    # Convert density and velocities to spherical coord.
+    vel = {}
+    for dim, axis in zip(['x','y','z'], [1,2,3]):
+        vel_ = ds['mom{}'.format(axis)]/ds.dens
+        vel[dim] = vel_ - vel_.sel(x=origin[0], y=origin[1], z=origin[2])
+    ds_sph = {}
+    ds_sph['vel1'], ds_sph['vel2'], ds_sph['vel3'] = to_spherical(vel.values(), origin)
+    ds_sph['rho'] = ds.dens.assign_coords(dict(r=ds_sph['vel1'].r))
+
+    # Radial binning
+    edges = np.insert(np.arange(ds.dx/2, rmax, ds.dx), 0, 0)
+    rprf = {}
+    for key, value in ds_sph.items():
+        rprf[key] = groupby_bins(value, 'r', edges)
+    for key in ('vel1', 'vel2', 'vel3'):
+        rprf[key+'_std'] = np.sqrt(groupby_bins(ds_sph[key]**2, 'r', edges))
+    rprf = xr.Dataset(rprf)
+    return rprf
 
 class Tools:
 
