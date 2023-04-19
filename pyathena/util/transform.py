52ec5fbc03ba8c4cac5762f10b92b4aa5ddd5858
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2023-04-18 20:55:17 -0400

    Update comments

--- pyathena/util/transform.py
+++ pyathena/util/transform.py
@@ -5,6 +5,8 @@ import xarray as xr
 def to_spherical(vec, origin):
     """Transform vector components from Cartesian to spherical coordinates
 
+    Assumes vec is a tuple of xarray.DataArray.
+
     Args:
         vec: tuple-like (vx, vy, vz) representing Cartesian vector components
         origin: tuple-like (x0, y0, z0) representing the origin of the spherical coords.
@@ -17,22 +19,27 @@ def to_spherical(vec, origin):
     vx, vy, vz = vec
     x0, y0, z0 = origin
     x, y, z = vx.x, vx.y, vx.z
-    # calculate spherical coordinates
+
+    # Calculate spherical coordinates
     R = np.sqrt((x-x0)**2 + (y-y0)**2)
     r = np.sqrt(R**2 + (z-z0)**2)
     th = np.arctan2(R, z-z0)
     ph = np.arctan2(y-y0, x-x0)
+    # Move branch cut [-pi, pi] -> [0, 2pi]
     ph = ph.where(ph >= 0, other=ph + 2*np.pi)
     sin_th, cos_th = R/r, (z-z0)/r
     sin_ph, cos_ph = (y-y0)/R, (x-x0)/R
+    # Avoid singularity
     sin_th.loc[dict(x=x0, y=y0, z=z0)] = 0
     cos_th.loc[dict(x=x0, y=y0, z=z0)] = 0
     sin_ph.loc[dict(x=x0, y=y0)] = 0
     cos_ph.loc[dict(x=x0, y=y0)] = 0
-    # transform vector components
+
+    # Transform Cartesian (vx, vy, vz) ->  spherical (v_r, v_th, v_phi)
     v_r = (vx*sin_th*cos_ph + vy*sin_th*sin_ph + vz*cos_th).rename('v_r')
     v_th = (vx*cos_th*cos_ph + vy*cos_th*sin_ph - vz*sin_th).rename('v_theta')
     v_ph = (-vx*sin_ph + vy*cos_ph).rename('v_phi')
+
     # assign spherical coordinates
     v_r.coords['r'] = r
     v_th.coords['r'] = r
