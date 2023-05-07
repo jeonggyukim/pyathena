94280176dbb7d507d00a7aeb499744b71216be5e
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2023-05-06 22:18:08 -0400

    Add task to calculate time averaged snapshot

--- pyathena/tigress_gc/tasks.py
+++ pyathena/tigress_gc/tasks.py
@@ -2,15 +2,18 @@
 
 # python modules
 from pathlib import Path
+import numpy as np
 
 # pyathena modules
 from pyathena.tigress_gc import tools
 
 def save_azimuthal_averages(s, overwrite=False):
-    """Calculates azimuthal averages and save to file
+    """
+    Calculates azimuthal averages and save to file
 
-    Args:
-        s: pyathena.LoadSim instance
+    Parameters
+    ----------
+    s : pyathena.LoadSim instance
     """
     for num in s.nums:
         fname = Path(s.basedir, 'azimuthal_averages_warmcold',
@@ -22,3 +25,43 @@ def save_azimuthal_averages(s, overwrite=False):
         print('processing model {} num {}'.format(s.basename, num))
         rprf = tools.calculate_azimuthal_averages(s, num, warmcold=True)
         rprf.to_netcdf(fname)
+
+
+def save_time_averaged_snapshot(s, ts, te, overwrite=False):
+    """
+    Generate time averaged snapshot between [ts, te]
+
+    Parameters
+    ----------
+    s : pyathena.LoadSim instance
+    ts : start time
+    te : end time
+    """
+
+    fname = Path(s.basedir, 'time_averages', 'prims.nc')
+    fname.parent.mkdir(exist_ok=True)
+    if fname.exists() and not overwrite:
+        print('File {} already exists; skipping...'.format(fname))
+        return
+
+    ns = tools.find_snapshot_number(s, ts)
+    ne = tools.find_snapshot_number(s, te)
+    nums = np.arange(ns, ne+1)
+
+    fields = ['density', 'velocity', 'pressure']
+
+    # load a first vtk
+    ds = s.load_vtk(nums[0], id0=False)
+    dat = ds.get_field(fields)
+
+    # loop through vtks
+    for num in nums[1:]:
+        ds = s.load_vtk(num, id0=False)
+        tmp = ds.get_field(fields)
+        dat += tmp
+    dat /= len(nums)
+    dat.attrs.update({'ts':s.load_vtk(ns).domain['time'],
+                      'te':s.load_vtk(ne).domain['time']})
+    if fname.exists():
+        fname.unlink()
+    dat.to_netcdf(fname)
