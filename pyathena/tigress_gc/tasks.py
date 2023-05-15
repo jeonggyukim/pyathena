065a993f1d3b8fde66ff485654bb5284d00739e5
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2023-05-15 16:21:49 -0400

    Add task to calculate ring masked averages

--- pyathena/tigress_gc/tasks.py
+++ pyathena/tigress_gc/tasks.py
@@ -3,9 +3,35 @@
 # python modules
 from pathlib import Path
 import numpy as np
+import xarray as xr
+import pandas as pd
 
 # pyathena modules
 from pyathena.tigress_gc import tools
+
+def save_ring_averages(s, Rmax, mf_crit=0.9, overwrite=False):
+    """
+    Calculates ring masked averages and save to file
+
+    Parameters
+    ----------
+    s : pyathena.LoadSim instance
+    """
+    fname = Path(s.basedir, 'ring_averages', 'gc_ring_average_warmcold.nc')
+    fname.parent.mkdir(exist_ok=True)
+    if fname.exists() and not overwrite:
+        print('File {} already exists; skipping...'.format(fname))
+        return
+
+    time, ravgs = [], []
+    for num in s.nums[100:]:
+        print('processing model {} num {}'.format(s.basename, num))
+        ravg = tools.calculate_ring_averages(s, num, Rmax, mf_crit, warmcold=True)
+        time.append(ravg.time)
+        ravgs.append(ravg)
+    ravgs = xr.concat(ravgs, dim=pd.Index(time, name='t'), combine_attrs='drop_conflicts')
+    ravgs.to_netcdf(fname)
+
 
 def save_azimuthal_averages(s, overwrite=False):
     """
