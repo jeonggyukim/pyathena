841c504ba8cfc780e2d0afab7af6a0c7a1ead195
Chang-Goo Kim <cgkim@astro.princeton.edu>
2023-07-20 12:30:23 -0400

    Add more volume renderings

--- pyathena/tigress_ncr/do_volume.py
+++ pyathena/tigress_ncr/do_volume.py
@@ -17,7 +17,116 @@ from pyathena.plt_tools.make_movie import make_movie
 
 import yt
 
+def add_fields(ds,xray=True):
+    # yt standard abundance fields
+    def _nHI(field, data):
+        return data[("gas","H_nuclei_density")]*data[("athena","xHI")]
+    def _nHII(field, data):
+        return data[("gas","H_nuclei_density")]*(1-data[("athena","xHI")]-2*data[("athena","xH2")])
+    def _nH2(field, data):
+        return 2.0*data[("gas","H_nuclei_density")]*data[("athena","xH2")]
+    def _LHalpha(field, data):
+        T4 = data[("athena","temperature")].v / 1.0e4
+        idx = (T4>0.1) * (T4<3)
+        alpha_eff = 1.17e-13 * T4 ** (-0.942 - 0.031 * np.log(T4))
+        hnu_halpha = 3.0268267464328148e-12
+        xHII = (1-data[("athena","xHI")]-2*data[("athena","xH2")])
+        return (alpha_eff * hnu_halpha * idx *
+                data[("gas","El_number_density")] *
+                data[("gas","H_p1_number_density")] *
+                data[("gas","cell_volume")] *
+                (yt.units.erg* yt.units.cm**3/yt.units.s)
+                )
+    # add/override fields
+    ds.add_field(("gas","H_p0_number_density"),function=_nHI, force_override=True,
+                units='cm**(-3)',display_name=r'$n_{\rm H^0}$', sampling_type="cell")
+    ds.add_field(("gas","H_p1_number_density"),function=_nHII, force_override=True,
+                units='cm**(-3)',display_name=r'$n_{\rm H^+}$', sampling_type="cell")
+    ds.add_field(("gas","H2_number_density"),function=_nH2, force_override=True,
+                units='cm**(-3)',display_name=r'$n_{\rm H_2}$', sampling_type="cell")
+    ds.add_field(("gas","H_alpha_luminosity"),function=_LHalpha, force_override=True,
+                units="erg/s",display_name=r'$L_{\rm H\alpha}$', sampling_type="cell")
 
+    # xray
+    if xray:
+        import pyxsim
+        emin=0.1
+        emax=10
+        nbins=1000
+        model='spex'
+        binscale='log'
+        Zmet = 1.0
+        srcmdl=pyxsim.CIESourceModel(model, emin, emax, nbins, Zmet,
+                                    binscale=binscale, abund_table='aspl')
+        xray_fields = srcmdl.make_source_fields(ds,0.5,7.0,force_override=True)
+    return ds
+
+def make_many_volumes(s,ds):
+    foutdir = osp.join(os.fspath(s.basedir), "volume")
+    fields = [("gas","xray_luminosity_0.5_7.0_keV"),
+              ("gas","H_alpha_luminosity"),
+            #   ("athena","rad_energy_density_PE"),
+            #   ("athena","rad_energy_density_PH"),
+              ("athena","specific_scalar[0]"),
+              ("athena","specific_scalar[1]"),
+              ("gas","H_nuclei_density"),
+              ("gas","H_p0_number_density"),
+              ("gas","H_p1_number_density"),
+              ("gas","H2_number_density")
+             ]
+    bounds = [(1.e24,1.e32),(1.e24,1.e30),
+            #   (1.e-3,1.e3),(1.e-15,1.e4),
+              (0.02,0.2),(0.01,1),
+              (1.e-4,1.e2),(1.e-4,1.e2),(1.e-4,1.e2),(1.e-4,1.e2)
+              ]
+    cmaps = ['winter','plasma',
+            #  'inferno','inferno',
+             'cool','cool',
+             'viridis','viridis','viridis','viridis'
+             ]
+    alphas = [np.linspace(20,50,5),np.linspace(20,50,5),
+              np.logspace(-1,2,5),np.logspace(-1,2,5),
+              np.linspace(20,50,5),np.linspace(20,50,5),np.linspace(20,50,5),np.linspace(50,100,5)
+              ]
+    for f, b, c, a in zip(fields,bounds, cmaps, alphas):
+        sc = yt.create_scene(ds,field=f)
+        # tfout = osp.join(foutdir, f"{f[1].replace('[','').replace(']','')}_tf_{num:04d}.png")
+        # sc[0].tfh.plot(tfout, profile_field=f)
+        tf = yt.ColorTransferFunction((np.log10(b[0]),np.log10(b[1])))
+        tf.add_layers(5, w=0.1, alpha=a, colormap=c)
+
+        sc[0].set_transfer_function(tf)
+
+        cam = sc.camera
+        cam.set_position([1024,-1024,1024],north_vector=[0,0,1])
+        cam.zoom(1.5)
+        sc.annotate_domain(ds,color=[1,1,1,1])
+
+        fout = osp.join(foutdir, f"{f[1].replace('[','').replace(']','')}_time_{num:04d}.png")
+        sc.save(fout)
+
+def make_joint_pdfs(s,ds):
+    foutdir = osp.join(os.fspath(s.basedir), "volume")
+
+    Nx, Ny, Nz = ds.domain_dimensions
+    le = ds.domain_left_edge.v
+    re = ds.domain_right_edge.v
+    profile = yt.create_profile(
+        data_source=ds.all_data(),
+        bin_fields=[("gas", "H_nuclei_density"), ("gas", "temperature")],
+        fields=[("gas","cell_volume"), ("gas", "cell_mass"),
+                ("gas","H_alpha_luminosity"),
+                ("athena","rad_energy_density_PE"),
+                ("athena","rad_energy_density_PH"),
+                ("athena","specific_scalar[0]"),
+                ("athena","specific_scalar[1]"),
+                ("gas","xray_luminosity_0.5_7.0_keV")
+               ],
+        n_bins=(256,256),
+        # units=dict(z="pc", velocity_z="km/s", volume="pc**3", mass="Msun"),
+        weight_field=None,
+        extrema=dict(H_nuclei_density=(1.e-6,1.e4),temperature=(10,1.e9)),
+    )
 def make_volume(ds):
     from yt.visualization.volume_rendering.api import Scene, create_volume_source
     box = ds.box(ds.domain_left_edge,ds.domain_right_edge)
@@ -83,16 +192,18 @@ if __name__ == "__main__":
 
     mynums = COMM.scatter(nums, root=0)
     print("[rank, mynums]:", COMM.rank, mynums)
-
+    foutdir = osp.join(os.fspath(s.basedir), "volume")
+    os.makedirs(foutdir, exist_ok=True)
     time0 = time.time()
     if True:
         for num in mynums:
             ds = s.ytload(num)
-            sc = make_volume(ds)
-            foutdir = osp.join(os.fspath(s.basedir), "volume")
-            os.makedirs(foutdir, exist_ok=True)
-            fout = osp.join(foutdir, f"time_{num:04d}.png")
-            sc.save(fout)
+            ds = add_fields(ds)
+            # sc = make_volume(ds)
+            # fout = osp.join(foutdir, f"time_{num:04d}.png")
+            # sc.save(fout)
+
+            sc = make_many_volumes(s,ds)
 
             n = gc.collect()
             print("Unreachable objects:", n, end=" ")
