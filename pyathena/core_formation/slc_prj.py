fc5ff4da31bffaaaf340d22401139e2f58522899
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2023-08-09 15:57:22 -0400

    Add SliceProj (add missing file)

---
+++ pyathena/core_formation/slc_prj.py
@@ -0,0 +1,84 @@
+# slc_prj.py
+
+import os
+import os.path as osp
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import astropy.units as au
+import astropy.constants as ac
+from matplotlib.colors import Normalize, LogNorm
+from mpl_toolkits.axes_grid1 import ImageGrid
+import xarray as xr
+
+from ..load_sim import LoadSim
+
+cmap_def = dict(
+    Sigma_gas=plt.cm.pink_r,
+)
+
+norm_def = dict(
+    Sigma_gas=(1e-1, 2e2),
+)
+
+class SliceProj:
+
+    @staticmethod
+    def _get_extent(domain):
+
+        r = dict()
+        r['x'] = (domain['le'][1], domain['re'][1],
+                  domain['le'][2], domain['re'][2])
+        r['y'] = (domain['le'][0], domain['re'][0],
+                  domain['le'][2], domain['re'][2])
+        r['z'] = (domain['le'][0], domain['re'][0],
+                  domain['le'][1], domain['re'][1])
+
+        return r
+
+    @LoadSim.Decorators.check_pickle
+    def read_prj(self, num, axes=['x', 'y', 'z'], prefix='prj',
+                 savdir=None, force_override=False):
+
+        axtoi = dict(x=0, y=1, z=2)
+        fields = ['dens']
+        axes = np.atleast_1d(axes)
+
+        ds = self.load_hdf5(num=num, quantities=fields)
+
+        res = dict()
+        res['extent'] = self._get_extent(self.domain)
+
+        for ax in axes:
+            i = axtoi[ax]
+            dx = self.domain['dx'][i]
+
+            res[ax] = dict()
+            res[ax]['Sigma_gas'] = (ds.dens*dx).sum(ax).data
+
+        return res
+
+    @staticmethod
+    def plt_proj(prj, axis='z', field='Sigma_gas', ax=None,
+                 cmap=None, norm=None, vmin=None, vmax=None):
+        try:
+            if cmap is None:
+                try:
+                    cmap = cmap_def[field]
+                except KeyError:
+                    cmap = plt.cm.viridis
+            if vmin is None or vmax is None:
+                vmin = norm_def[field][0]
+                vmax = norm_def[field][1]
+
+            if norm is None or norm == 'log':
+                norm = mpl.colors.LogNorm(vmin=vmin, vmax=vmax)
+            elif norm == 'linear':
+                norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+
+            if ax is not None:
+                plt.sca(ax)
+            plt.imshow(prj[axis][field], cmap=cmap, extent=prj['extent'][axis],
+                      norm=norm, origin='lower', interpolation='none')
+        except KeyError:
+            pass
