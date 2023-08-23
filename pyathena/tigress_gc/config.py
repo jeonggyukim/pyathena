c4641b91d1ed23802caf18885fb2ea291accb85e
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2023-08-23 13:16:46 -0400

    Make maximum cylindrical radius as config parameter

--- pyathena/tigress_gc/config.py
+++ pyathena/tigress_gc/config.py
@@ -1,3 +1,13 @@
 Twarm = 2e4
 muH = 1.4271
 NUM_START = 100
+
+# Maximum radius to excise inflowing stream gas
+Rmax = dict(L0=710,
+            L1=710,
+            L2=760,
+            L3=870,
+            S0=185,
+            S1=205,
+            S2=245,
+            S3=245)
