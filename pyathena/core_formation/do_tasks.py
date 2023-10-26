65b4eac52f917146fec37bdde522fb56e8809f13
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2023-10-26 16:18:59 -0400

    Add models

--- pyathena/core_formation/do_tasks.py
+++ pyathena/core_formation/do_tasks.py
@@ -8,8 +8,8 @@ from pyathena.core_formation import config, tasks, tools
 
 if __name__ == "__main__":
     # load all models
-    m1 = {f"M5J2P{iseed}N512": f"/scratch/gpfs/sm69/cores/new/M5.J2.P{iseed}.N512" for iseed in range(0, 35)}
-    m2 = {f"M10J4P{iseed}N1024": f"/scratch/gpfs/sm69/cores/new/M10.J4.P{iseed}.N1024" for iseed in range(0, 5)}
+    m1 = {f"M5J2P{iseed}N512": f"/scratch/gpfs/sm69/cores/new/M5.J2.P{iseed}.N512" for iseed in range(0, 40)}
+    m2 = {f"M10J4P{iseed}N1024": f"/scratch/gpfs/sm69/cores/new/M10.J4.P{iseed}.N1024" for iseed in range(0, 7)}
     m3 = {f"M5J2P{iseed}N256": f"/scratch/gpfs/sm69/cores/new/M5.J2.P{iseed}.N256" for iseed in range(0, 1)}
     models = {**m1, **m2, **m3}
 #    models['M10J4P3N1024newsink'] = "/scratch/gpfs/sm69/cores/M10.J4.P3.N1024.newsink"
