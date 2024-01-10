4507efe571da23b879da2199e67246f302e198f6
Jeong-Gyu Kim <kimjg@astro.princeton.edu>
2024-01-10 05:00:59 -0500

    sixray_test : update of script for HI-H2 transition test with CR.

--- pyathena/sixray_test/hih2.py
+++ pyathena/sixray_test/hih2.py
@@ -28,17 +28,17 @@ def NHtrans_NHItot_alphaG_B16(nH, chi, R=3.0e-17, sigmad=1.9e-21, D0_ISRF=5.7e-1
     (NHtrans, NHItot, alphaG) : tuple
     """
 
-    
+
     beta = 0.7
     sg = sigmad/1.9e-21
     sd = 2.36e-3
     alpha = chi*D0_ISRF/(R*nH)
     Wgtot = 8.8e13/(1.0 + 8.9*sg)**0.37
     G = Wgtot*(sg*1.9e-21)/sd
-    
+
     res = (beta*np.log((alpha*G/col_factor)**(1.0/beta) + 1.0)/sigmad,
            np.log((alpha*G/col_factor) + 1.0)/sigmad,
-           alpha*G)
+           alpha*G,alpha,G)
 
     return res
 
@@ -57,26 +57,34 @@ def analyze_HIH2_all(sa, models=None):
         rr[mdl] = pd.DataFrame(r)
 
     df = pd.concat([v for k,v in rr.items()])
-    
+
     return df, dd
 
 def analyze_HIH2(s, mdl, dd):
-    
+
     from scipy import interpolate
-    
+
     NHtrans = []
     NHItot = []
+    NHItot22 = [] # HI column density upto NH<10^22/cm^2
+    NHItot23 = [] # HI column density upto NH<10^23/cm^2
+    NHItot24 = [] # HI column density upto NH<10^24/cm^2
     nH = []
     chi = []
     dx = []
     dx_cgs = []
     alphaG = []
+    alpha = []
+    G = []
     NHtrans_B16 = []
     NHItot_B16 = []
     xi_CR0 = []
     nx = s.par['domain1']['Nx3']
     xHI_final = []
-    
+    xicr_over_RnH_trans = []
+    zeta_pd_over_RnH_trans = []
+    fshld_H2_trans = []
+
     NH0 = 1.87e21
     for log_chi_PE in dd[mdl].log_chi_PE.data:
         for log_nH in dd[mdl].log_nH.data:
@@ -90,40 +98,80 @@ def analyze_HIH2(s, mdl, dd):
             d['NHI'] = d['nHI'].cumsum()*dx_cgs_
             d['Av'] = Zd*d['NH']/NH0
             d['2xH2'] = 2.0*d['xH2']
-            
+
             dx.append(s.domain['dx'][2])
             dx_cgs.append(dx_cgs_)
             nH.append(10.0**log_nH)
             chi.append(10.0**log_chi_PE)
             xi_CR0.append(s.par['problem']['xi_CR0'])
             xHI_final.append((d['xHI'].data)[-1])
-            NHtrans_B16_, NHItot_B16_, alphaG_ = NHtrans_NHItot_alphaG_B16(
+            NHtrans_B16_, NHItot_B16_, alphaG_, alpha_, G_ = NHtrans_NHItot_alphaG_B16(
                 10.0**log_nH, 10.0**log_chi_PE, R, sigmad, D0_ISRF)
 
             NHtrans_B16.append(NHtrans_B16_)
             NHItot_B16.append(NHItot_B16_)
             alphaG.append(alphaG_)
-            
+            alpha.append(alpha_)
+            G.append(G_)
+
             try:
                 x = np.log10(d['NH']).data
                 y = d['2xH2'].data
-                x_ = np.linspace(x.min(),x.max(),100000)
+                # ratio between CRIR and formation at Ntrans
+                y2 = d['xi_CR']/(R*d['nH'])
+                # self-shielding factor fshld_H2 at Ntrans
+                y3 = d['chi_H2_ext']/10.0**log_chi_PE
+                # ratio btw PD and formation at Ntrans
+                y4 = d['chi_H2_ext']*s.par['cooling']['xi_diss_H2_ISRF']/(R*d['nH'])
+                x_ = np.linspace(x.min(),x.max(),500000)
                 f = interpolate.interp1d(x, y, kind='linear')
+                f2 = interpolate.interp1d(x, y2, kind='linear')
+                f3 = interpolate.interp1d(x, y3, kind='linear')
+                f4 = interpolate.interp1d(x, y4, kind='linear')
                 y_ = f(x_)
                 #NHtrans.append(10.0**x_[np.where(y_ >= 0.5)[0][0]])
                 NHtrans.append(10.0**x_[np.where(y_ < 0.5)[-1][-1]])
                 NHItot.append(d['NHI'][-1])
+                xicr_over_RnH_trans.append(f2(x_[np.where(y_ < 0.5)[-1][-1]]))
+                fshld_H2_trans.append(f3(x_[np.where(y_ < 0.5)[-1][-1]]))
+                zeta_pd_over_RnH_trans.append(f4(x_[np.where(y_ < 0.5)[-1][-1]]))
             except IndexError:
                 NHtrans.append(np.nan)
                 NHItot.append(np.nan)
-    
+                xicr_over_RnH_trans.append(np.nan)
+                fshld_H2_trans.append(np.nan)
+                zeta_pd_over_RnH_trans.append(np.nan)
+
+            x = np.log10(d['NH']).data
+            y = np.log10(d['NHI']).data
+            x_ = np.linspace(x.min(),x.max(),100000)
+            f = interpolate.interp1d(x, y, kind='linear')
+            y_ = f(x_)
+            try:
+                NHItot22.append(10.0**y_[np.where(x_ < 22.0)[-1][-1]])
+            except IndexError:
+                NHItot22.append(np.nan)
+            try:
+                NHItot23.append(10.0**y_[np.where(x_ < 23.0)[-1][-1]])
+            except IndexError:
+                NHItot23.append(np.nan)
+            try:
+                NHItot24.append(10.0**y_[np.where(x_ < 24.0)[-1][-1]])
+            except IndexError:
+                NHItot24.append(np.nan)
+
     r = dict()
     r['nH'] = np.array(nH)
     r['dx'] = np.array(dx)
     r['dx_cgs'] = np.array(dx_cgs)
     r['NHtrans'] = np.array(NHtrans)
     r['NHItot'] = np.array(NHItot)
+    r['NHItot22'] = np.array(NHItot22)
+    r['NHItot23'] = np.array(NHItot23)
+    r['NHItot24'] = np.array(NHItot24)
     r['alphaG'] = np.array(alphaG)
+    r['alpha'] = np.array(alpha)
+    r['G'] = np.array(G)
     r['NHtrans_B16'] = np.array(NHtrans_B16)
     r['NHItot_B16'] = np.array(NHItot_B16)
     r['chi'] = np.array(chi)
@@ -131,5 +179,8 @@ def analyze_HIH2(s, mdl, dd):
     r['NHmax'] = r['nH']*r['dx_cgs']*nx
     r['xi_CR0'] = np.array(xi_CR0)
     r['xHI_final'] = xHI_final
-    
+    r['xicr_over_RnH_trans'] = np.array(xicr_over_RnH_trans)
+    r['fshld_H2_trans'] = np.array(fshld_H2_trans)
+    r['zeta_pd_over_RnH_trans'] = np.array(zeta_pd_over_RnH_trans)
+
     return r
