219bd5c0b81d7fe46702c452fedfb76b3aa2be76
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2024-10-09 11:18:17 -0400

    Change sim dat directory

--- pyathena/core_formation/do_tasks.py
+++ pyathena/core_formation/do_tasks.py
@@ -9,9 +9,10 @@ from pyathena.core_formation import config, tasks, tools
 if __name__ == "__main__":
     # load all models
     m1 = {f"M5J2P{iseed}N512": f"/scratch/gpfs/sm69/cores/hydro/M5.J2.P{iseed}.N512" for iseed in range(0, 40)}
-    m2 = {f"M10J4P{iseed}N1024": f"/scratch/gpfs/sm69/cores/hydro/M10.J4.P{iseed}.N1024" for iseed in range(0, 7)}
-    m3 = {f"M5J2P{iseed}N256": f"/scratch/gpfs/sm69/cores/hydro/M5.J2.P{iseed}.N256" for iseed in range(0, 1)}
-    models = {**m1, **m2, **m3}
+    m2 = {f"M10J4P{iseed}N1024": f"/scratch/gpfs/sm69/cores/hydro/M10.J4.P{iseed}.N1024" for iseed in range(0, 3)}
+    m3 = {f"M10J4P{iseed}N1024": f"/projects2/EOSTRIKE/coreform_MO2024/M10.J4.P{iseed}.N1024" for iseed in range(3, 7)}
+    m4 = {f"M5J2P{iseed}N256": f"/scratch/gpfs/sm69/cores/hydro/M5.J2.P{iseed}.N256" for iseed in range(0, 1)}
+    models = {**m1, **m2, **m3, **m4}
 
     # MHD models
     for iseed in [1,]:
@@ -218,11 +219,11 @@ if __name__ == "__main__":
                 with Pool(args.np) as p:
                     p.map(wrapper2, s.good_cores())
 
-#            def wrapper2(pid):
-#                ncrit = s.cores[pid].attrs['numcrit']
-#                tasks.calculate_linewidth_size(s, ncrit, pid=pid, overwrite=args.overwrite)
-#            with Pool(args.np) as p:
-#                p.map(wrapper2, s.good_cores())
+            def wrapper3(pid):
+                ncrit = s.cores[pid].attrs['numcrit']
+                tasks.calculate_linewidth_size(s, ncrit, pid=pid, overwrite=args.overwrite)
+            with Pool(args.np) as p:
+                p.map(wrapper3, s.good_cores())
 
         # make plots
         if args.plot_core_evolution:
