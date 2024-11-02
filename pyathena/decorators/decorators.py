ed536b5fbfd5642d5f9d259faba529f0bc95d176
Jeong-Gyu Kim <kimjg@astro.princeton.edu>
2024-11-01 22:19:56 -0400

    Decorators for pickle (hst and others) and netcdf (e.g., zprof)

--- pyathena/decorators/decorators.py
+++ pyathena/decorators/decorators.py
@@ -148,3 +148,77 @@ def check_netcdf_zprof_vtk(func):
             return ds
 
     return wrapper
+
+def check_netcdf(func):
+
+    @functools.wraps(func)
+    def wrapper(cls, *args, **kwargs):
+        # Convert positional args to keyword args
+        call_args = getcallargs(func, cls, *args, **kwargs)
+        call_args.pop('self')
+        kwargs = call_args
+
+        if 'num' in kwargs:
+            read_all = False
+        else:
+            read_all = True
+
+        prefix = kwargs['prefix']
+
+        if 'savdir' in kwargs:
+            savdir = kwargs['savdir']
+            if savdir is None:
+                savdir = osp.join(cls.savdir, prefix)
+        else:
+            savdir = osp.join(cls.savdir, prefix)
+
+        if 'force_override' in kwargs:
+            force_override = kwargs['force_override']
+        else:
+            force_override = False
+
+        # Create savdir if it doesn't exist
+        if not osp.exists(savdir):
+            try:
+                os.makedirs(savdir)
+            except FileExistsError:
+                pass
+            force_override = True
+
+        if not read_all:
+            fnetcdf = '{0:s}.{1:s}.{2:s}.{3:04d}.nc'.\
+                format(cls.problem_id, cls.basename,
+                       prefix, kwargs['num'])
+        else:
+            read_all = True
+            fnetcdf = '{0:s}.{1:s}.{2:s}.all.nc'.\
+                format(cls.problem_id, cls.basename,
+                       prefix)
+
+        fnetcdf = osp.join(savdir, fnetcdf)
+        if not force_override and osp.exists(fnetcdf):
+            if read_all:
+                cls.logger.info(
+                    'Read {0:s} from existing NetCDF dump.'.format(prefix) +\
+                    ' Set force_override to True if time range should be updated.')
+
+            ds = xr.open_dataset(fnetcdf)
+            return ds
+        else:
+            cls.logger.info('Construct directly without reading from NetCDF')
+            ds = func(cls, **kwargs)
+
+            # Somehow overwriting with mode='w' in to_netcdf doesn't work..
+            # Delete file first
+            if osp.exists(fnetcdf):
+                os.remove(fnetcdf)
+
+            try:
+                ds.to_netcdf(fnetcdf, mode='w')
+            except (IOError, PermissionError) as e:
+                cls.logger.warning('Could not netcdf to {0:s}.'\
+                                   .format(fnetcdf))
+
+            return ds
+
+    return wrapper
