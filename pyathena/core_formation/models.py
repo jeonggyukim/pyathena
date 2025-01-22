02dca5e3c05b39294bcfa87ebdf177e595b1117d
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2025-01-22 18:06:14 -0500

    Add MHD models

--- pyathena/core_formation/models.py
+++ pyathena/core_formation/models.py
@@ -1,6 +1,8 @@
 m1 = {f"M5J2P{iseed}N512": f"/scratch/gpfs/sm69/cores/hydro/M5.J2.P{iseed}.N512" for iseed in range(0, 40)}
 m2 = {f"M10J4P{iseed}N1024": f"/scratch/gpfs/sm69/cores/hydro/M10.J4.P{iseed}.N1024" for iseed in range(0, 3)}
-m3 = {f"M10J4P{iseed}N1024": f"/projects2/EOSTRIKE/coreform_MO2024/M10.J4.P{iseed}.N1024" for iseed in range(3, 7)}
+m3 = {f"M10J4P{iseed}N1024": f"/projects2/EOSTRIKE/sanghyuk/cores/M10.J4.P{iseed}.N1024" for iseed in range(3, 7)}
 mach5 = m1
 mach10 = {**m2, **m3}
-models = {**mach5, **mach10}
+hydro = {**mach5, **mach10}
+mhd = {f"M10J4B4P{iseed}N1024": f"/scratch/gpfs/sm69/cores/mhd/M10.J4.B4.P{iseed}.N1024" for iseed in range(0, 2)}
+models = {**hydro, **mhd}
