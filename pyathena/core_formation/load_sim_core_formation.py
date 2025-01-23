dc332a3f33ae397f8a0697e33eb591328d8f7787
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2025-01-23 14:31:25 -0500

    Comment out unused code
    This raises error when there is no output at all (only history)
    so better to comment out for now

--- pyathena/core_formation/load_sim_core_formation.py
+++ pyathena/core_formation/load_sim_core_formation.py
@@ -94,9 +94,9 @@ class LoadSimCoreFormation(LoadSim, Hst, SliceProj, LognormalPDF,
             LognormalPDF.__init__(self, self.Mach)
             TimingReader.__init__(self, self.basedir, self.problem_id)
 
-            # Set nums dictionary (when hdf5 is stored in elsewhere for storage reasons)
-            if self.nums is None:
-                self.nums = self.nums_partab['par0']
+#            # Set nums dictionary (when hdf5 is stored in elsewhere for storage reasons)
+#            if self.nums is None:
+#                self.nums = self.nums_partab['par0']
 
             # Set domain
             Lbox = set(self.domain['Lx'])
