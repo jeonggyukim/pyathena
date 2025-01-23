3fc033e1224433569d32aeb64317ea6f1a908f7d
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2025-01-23 18:12:42 -0500

    Update model

--- pyathena/core_formation/models.py
+++ pyathena/core_formation/models.py
@@ -1,6 +1,6 @@
 m1 = {f"M5J2P{iseed}N512": f"/scratch/gpfs/sm69/cores/hydro/M5.J2.P{iseed}.N512" for iseed in range(0, 40)}
-m2 = {f"M10J4P{iseed}N1024": f"/scratch/gpfs/sm69/cores/hydro/M10.J4.P{iseed}.N1024" for iseed in range(0, 3)}
-m3 = {f"M10J4P{iseed}N1024": f"/projects2/EOSTRIKE/sanghyuk/cores/M10.J4.P{iseed}.N1024" for iseed in range(3, 7)}
+m2 = {f"M10J4P{iseed}N1024": f"/scratch/gpfs/sm69/cores/hydro/M10.J4.P{iseed}.N1024" for iseed in range(0, 2)}
+m3 = {f"M10J4P{iseed}N1024": f"/projects2/EOSTRIKE/sanghyuk/cores/M10.J4.P{iseed}.N1024" for iseed in range(2, 7)}
 mach5 = m1
 mach10 = {**m2, **m3}
 hydro = {**mach5, **mach10}
