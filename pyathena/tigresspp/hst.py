4c06d4cce1aeee639c3619f4804d99d92bc06042
Chang-Goo Kim <cgkim@astro.princeton.edu>
2025-01-27 14:51:42 -0500

    More history including mass conservation

--- pyathena/tigresspp/hst.py
+++ pyathena/tigresspp/hst.py
@@ -62,6 +62,12 @@ class Hst:
         if "p0m" in hst:
             h["mass_sp"] = hst["p0m"] * u.Msun
             h["Sigma_sp"] = h["mass_sp"] / (LxLy * u.pc**2)
+            if "msp_removed" in hst:
+                h["mass_sp_rm"] = hst["msp_removed"] * u.Msun
+                h["Sigma_sp_rm"] = h["mass_sp_rm"] / (LxLy * u.pc**2)
+        if "mass_loss_upper" in hst:
+            h["mass_out"] = (hst["mass_loss_upper"]-hst["mass_loss_lower"]) * u.Msun
+            h["Sigma_out"] = h["mass_out"] / (LxLy * u.pc**2)
         # Mass surface density in Msun/pc^2
         h["Sigma_gas"] = h["mass"] / (LxLy * u.pc**2)
 
@@ -110,29 +116,41 @@ class Hst:
         self.hst = h
 
         # SN data
-        if "sn" in self.files:
-            if osp.exists(self.files["sn"]):
-                sn = pd.read_csv(self.files["sn"])
-                snr = get_snr(sn["time"] * self.u.Myr, hst["time"] * self.u.Myr)
+        # if "sn" in self.files:
+        #     if osp.exists(self.files["sn"]):
+        #         sn = pd.read_csv(self.files["sn"])
+        #         snr = get_snr(sn["time"] * self.u.Myr, hst["time"] * self.u.Myr)
 
-                self.sn = sn
-                self.snr = snr / LxLy
+        #         self.sn = sn
+        #         self.snr = snr / LxLy
 
         return h
 
     def plt_hst(self):
         h = self.read_hst()
-        fig, axes = plt.subplots(1, 3, figsize=(8, 3), num=0)
+        if "mass_loss_upper" in h:
+            fig, axes = plt.subplots(2, 3, figsize=(8, 6), num=0)
+        else:
+            fig, axes = plt.subplots(1, 3, figsize=(8, 3), num=0)
+        axes = axes.flatten()
         plt.sca(axes[0])
-        plt.plot(h["time"], h["Sigma_gas"])
+        plt.plot(h["time"], h["Sigma_gas"], label="gas")
         if "Sigma_sp" in h:
-            plt.plot(h["time"], h["Sigma_sp"])
-        plt.ylabel(r"$\Sigma_{\rm gas}, \Sigma_{\rm sink}$")
+            plt.plot(h["time"], h["Sigma_sp"], label="sink")
+        if "Sigma_sp" in h:
+            plt.plot(h["time"], h["Sigma_sp"]+h["Sigma_sp_rm"], label="sink,total")
+        if "Sigma_out" in h:
+            plt.plot(h["time"], h["Sigma_out"], label="out")
+        plt.ylabel(r"$\Sigma$")
+        plt.xlabel("time")
+        plt.legend(fontsize="xx-small",frameon=False)
 
         plt.sca(axes[1])
         plt.plot(h["time"], h["sfr10"])
         plt.plot(h["time"], h["sfr40"])
         plt.ylabel(r"$\Sigma_{\rm SFR}$")
+        plt.xlabel("time")
+
         plt.yscale("log")
 
         if "SFUV" in h:
@@ -140,8 +158,34 @@ class Hst:
             plt.plot(h["time"], h["SFUV"])
             plt.plot(h["time"], h["heat_ratio"])
             plt.ylabel(r"$\Sigma_{\rm FUV}, \Sigma_{\rm FUV}/\Sigma_{\rm FUV,0}$")
+            plt.xlabel("time")
             plt.yscale("log")
+
+        if "mass_loss_upper" in h:
+            dm = h["mass"]-h["mass"][0]
+            mloss = h["mass_out"]
+            dmsp = h["mass_sp"] - h["mass_sp"][0]
+            dmsp_removed = h["mass_sp_rm"]
+            mtot = h["mass"][0]
+
+            plt.sca(axes[3])
+            plt.plot(h["time"],dm,label=r"$\Delta m_{\rm gas}$")
+            plt.plot(h["time"],-dmsp-dmsp_removed-mloss, label=r"$-\Delta m_{\rm sink}-\Delta m_{\rm sink,rm}-m_{\rm out}$",ls=":")
+            plt.plot(h["time"],dmsp, label=r"$\Delta m_{\rm sink}$")
+            plt.plot(h["time"],dmsp_removed, label=r"$\Delta m_{\rm sink,rm}$")
+            plt.plot(h["time"],mloss, label=r"$\Delta m_{\rm out}$")
+            plt.axhline(0,ls="--",color="k")
+            plt.legend(fontsize="xx-small",frameon=False)
+            plt.xlabel("time")
+            plt.ylabel(r"mass [$M_\odot$]")
+
+            plt.sca(axes[4])
+            plt.plot(h["time"],((dm)+dmsp+dmsp_removed+mloss)/mtot,ls=":")
+            plt.xlabel("time")
+            plt.ylabel(r"$(\Delta m_{\rm gas}+\Delta m_{\rm sink}+\Delta m_{\rm sink,rm}$"
+                    r"$+\Delta m_{\rm out})/m_{\rm gas}(0)$")
         plt.tight_layout()
+
         return fig
 
 
