adc8ee9f707f7a78901ca9824acb04a0fdec2772
Chang-Goo Kim <cgkim@astro.princeton.edu>
2025-03-21 12:32:15 -0400

    Updated a problem specific script

---
+++ pyathena/tigresspp/plot_slices.py
@@ -0,0 +1,175 @@
+import sys
+
+sys.path.insert(0, "../")
+import os
+import os.path as osp
+import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize, LogNorm, SymLogNorm
+import cmasher as cmr
+import numpy as np
+
+from pyathena.tigresspp.load_sim_tigresspp import LoadSimTIGRESSPP
+from pyathena.fields.fields import DerivedFields
+
+from mpi4py import MPI
+
+
+def plot_slice_xy(sim, slc, field, dfi, vec=None, stream_kwargs=dict(color="k")):
+    dfi_ = dfi[field]
+    data = dfi_["func"](slc, sim.u)
+    if field == "Fcr3":
+        data = np.abs(data)
+    im = plt.pcolormesh(
+        data.x,
+        data.y,
+        data,
+        cmap=dfi_["imshow_args"]["cmap"],
+        norm=dfi_["imshow_args"]["norm"],
+    )
+    if vec is not None:
+        vx = f"{vec}1"
+        vy = f"{vec}2"
+        plt.streamplot(
+            slc.x.data, slc.y.data, slc[vx].data, slc[vy].data, **stream_kwargs
+        )
+    plt.gca().set_aspect("equal", adjustable="box")
+    plt.xlim(sim.domain["le"][0], sim.domain["re"][0])
+    plt.ylim(sim.domain["le"][1], sim.domain["re"][1])
+    return im
+
+
+def plot_slice_xz(sim, slc, field, dfi, vec=None, stream_kwargs=dict(color="k")):
+    dfi_ = dfi[field]
+    data = dfi_["func"](slc, sim.u)
+    if field == "Fcr3":
+        data = np.abs(data)
+    im = plt.pcolormesh(
+        data.x,
+        data.z,
+        data,
+        cmap=dfi_["imshow_args"]["cmap"],
+        norm=dfi_["imshow_args"]["norm"],
+    )
+    if vec is not None:
+        vx = f"{vec}1"
+        vy = f"{vec}3"
+        plt.streamplot(
+            slc.x.data, slc.z.data, slc[vx].data, slc[vy].data, **stream_kwargs
+        )
+    plt.gca().set_aspect("equal", adjustable="box")
+    plt.xlim(sim.domain["le"][0], sim.domain["re"][0])
+    plt.ylim(sim.domain["le"][2], sim.domain["re"][2])
+    return im
+
+
+def plot_slices(sim, num, savefig=True):
+    slc_xy = sim.get_slice(num, "allslc.z", slc_kwargs=dict(z=0, method="nearest"))
+    slc_xz = sim.get_slice(num, "allslc.y", slc_kwargs=dict(y=0, method="nearest"))
+    xz_ratio = sim.domain["Lx"][2] / sim.domain["Lx"][1]
+    flist = [
+        "nH",
+        "T",
+        "vmag",
+        "Vcr_mag",
+        "sigma_para",
+        "pok_cr",
+        "pok",
+        "Fcr3",
+        "Bmag",
+    ]
+    vectors = [
+        None,
+        None,
+        "velocity",
+        "0-Vc",
+        None,
+        None,
+        None,
+        "0-Fc",
+        "cell_centered_B",
+    ]
+    nf = len(flist)
+    fig, axes = plt.subplots(
+        2,
+        nf,
+        figsize=(2 * nf, 2 * (1 + xz_ratio)),
+        gridspec_kw=dict(height_ratios=[xz_ratio, 1]),
+        constrained_layout=True,
+        num=0,
+    )
+    for ax, field, vec in zip(axes[0, :], flist, vectors):
+        plt.sca(ax)
+        im = plot_slice_xz(
+            sim,
+            slc_xz,
+            field,
+            sim.dfi,
+            vec=vec,
+            stream_kwargs=dict(color="w" if field == "Bmag" else "k"),
+        )
+        cbar = plt.colorbar(
+            im,
+            orientation="horizontal",
+            location="top",
+            pad=0.01,
+            shrink=0.8,
+            aspect=10,
+            label=sim.dfi[field]["label"],
+        )
+        ax.set_aspect("equal", adjustable="box")
+        ax.axis("off")
+    for ax, field, vec in zip(axes[1, :], flist, vectors):
+        plt.sca(ax)
+        im = plot_slice_xy(
+            sim,
+            slc_xy,
+            field,
+            sim.dfi,
+            vec=vec,
+            stream_kwargs=dict(density=0.5, color="w" if field == "Bmag" else "k"),
+        )
+        ax.set_aspect("equal", adjustable="box")
+        ax.axis("off")
+    tMyr = slc_xy.attrs["time"] * sim.u.Myr
+    axes[0, 0].annotate(
+        f"t={tMyr:.2f} Myr",
+        (0.5, 0.99),
+        ha="center",
+        va="top",
+        xycoords="axes fraction",
+        fontsize="x-large",
+        bbox=dict(boxstyle="round,pad=0.2", fc="w", ec="k", lw=1),
+    )
+
+    if savefig:
+        savdir = osp.join(sim.savdir, "cr_snapshot")
+        if not osp.exists(savdir):
+            os.makedirs(savdir, exist_ok=True)
+
+        savname = osp.join(savdir, "{0:s}_{1:04d}.png".format(sim.basename, num))
+        plt.savefig(savname, dpi=200, bbox_inches="tight")
+    return fig
+    # plt.tight_layout()
+
+
+if __name__ == "__main__":
+    spp = LoadSimTIGRESSPP(sys.argv[1])
+    dfi = DerivedFields(spp.par)
+    dfi.dfi["T"]["imshow_args"]["cmap"] = "Spectral_r"
+    dfi.dfi["T"]["imshow_args"]["norm"] = LogNorm(vmin=1e2, vmax=1e8)
+    dfi.dfi["nH"]["imshow_args"]["cmap"] = cmr.rainforest
+    dfi.dfi["nH"]["imshow_args"]["norm"] = LogNorm(vmin=1e-4, vmax=1e2)
+    dfi.dfi["vmag"]["imshow_args"]["cmap"] = dfi.dfi["Vcr_mag"]["imshow_args"]["cmap"]
+    dfi.dfi["vmag"]["imshow_args"]["norm"] = dfi.dfi["Vcr_mag"]["imshow_args"]["norm"]
+    dfi.dfi["pok"]["imshow_args"]["cmap"] = dfi.dfi["pok_cr"]["imshow_args"]["cmap"]
+    dfi.dfi["pok"]["imshow_args"]["norm"] = dfi.dfi["pok_cr"]["imshow_args"]["norm"]
+    spp.dfi = dfi.dfi
+
+    COMM = MPI.COMM_WORLD
+    mynums = [spp.nums[i] for i in range(len(spp.nums)) if i % COMM.size == COMM.rank]
+    print(COMM.rank, mynums)
+
+    for num in mynums:
+        print(num)
+        f = plot_slices(spp, num)
+        plt.close(f)
