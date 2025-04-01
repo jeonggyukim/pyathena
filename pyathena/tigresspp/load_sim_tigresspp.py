80630e69624c49f943200c84a9b0f10afd6db7b1
Chang-Goo Kim <cgkim@astro.princeton.edu>
2025-03-31 22:15:41 -0400

    Updated zprof reader to handle vz separation

--- pyathena/tigresspp/load_sim_tigresspp.py
+++ pyathena/tigresspp/load_sim_tigresspp.py
@@ -284,22 +284,43 @@ class LoadSimTIGRESSPP(LoadSim,Hst,Timing):
                 mtime = max(osp.getmtime(f),mtime)
             return max(mtime,osp.getmtime(__file__))
 
-        dlist = dict()
+        if self.ff.zprof_separate_vz:
+            dlist_pvz = dict()
+            dlist_nvz = dict()
+        else:
+            dlist = dict()
         for fname in self.files["zprof"]:
             with open(fname, "r") as f:
                 header = f.readline()
             data = pd.read_csv(fname, skiprows=1)
+            tmp = data.pop("k")
             data.index = data.x3v
             time = eval(
                 header[header.find("time") : header.find("cycle")]
                 .split("=")[-1]
                 .strip()
             )
-            phase = (
-                header[header.find("phase") : header.find("variable")]
-                .split("=")[-1]
-                .strip()
-            )
+            if self.ff.zprof_separate_vz:
+                phase = (
+                    header[header.find("phase") : header.find("vz_dir")]
+                    .split("=")[-1]
+                    .strip()
+                )
+                vz = eval(
+                    header[header.find("vz_dir") : header.find("variable")]
+                    .split("=")[-1]
+                    .strip()
+                )
+                if vz>0:
+                    dlist = dlist_pvz
+                else:
+                    dlist = dlist_nvz
+            else:
+                phase = (
+                    header[header.find("phase") : header.find("variable")]
+                    .split("=")[-1]
+                    .strip()
+                )
             for ph in self.phase:
                 if ph in fname:
                     if phase not in dlist:
@@ -308,10 +329,19 @@ class LoadSimTIGRESSPP(LoadSim,Hst,Timing):
                         data.to_xarray().assign_coords(time=time).rename(x3v="z")
                     )
 
-        dset = []
-        for phase in dlist:
-            dset.append(xr.concat(dlist[phase], dim="time").assign_coords(phase=phase))
-        dset = xr.concat(dset, dim="phase")
+        if self.ff.zprof_separate_vz:
+            dset_vz = []
+            for vz_dir,dlist in zip([1,-1],[dlist_pvz,dlist_nvz]):
+                dset = []
+                for phase in dlist:
+                    dset.append(xr.concat(dlist[phase], dim="time").assign_coords(phase=phase))
+                dset_vz.append(xr.concat(dset, dim="phase").assign_coords(vz_dir=vz_dir))
+            dset = xr.concat(dset_vz, dim="vz_dir")
+        else:
+            dset = []
+            for phase in dlist:
+                dset.append(xr.concat(dlist[phase], dim="time").assign_coords(phase=phase))
+            dset = xr.concat(dset, dim="phase")
 
         return dset
 
