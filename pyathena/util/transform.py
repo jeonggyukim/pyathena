9e295625ad6f1c771b4c916ab0ede34f37079b32
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2025-04-18 08:51:03 -0400

    Remove fast-histogram dependency

--- pyathena/util/transform.py
+++ pyathena/util/transform.py
@@ -2,7 +2,6 @@ import numpy as np
 import xarray as xr
 import dask.array as da
 from scipy.spatial.transform import Rotation
-import fast_histogram as fh
 import warnings
 
 def euler_rotation(vec, angles):
